69ad183f777eff0d1a2f9d34f72c72e8f7309b4c
DarioBenjaminMontero <dariomontero2008@gmail.com>
2026-06-18 09:19:52 -0300

    cambios

--- Sequelize/controladores/hospitalesControladores.js
+++ Sequelize/controladores/hospitalesControladores.js
@@ -1,13 +1,12 @@
 const { Hospitales } = require("../modelos/hospitalesModelos.js")
 
 const obtenerHospitales = async(req, res) =>{
-
     const consulta = await Hospitales.findAll()
 res.status(200).json(consulta);
 }
 
-const crearHospital = async(req, res) =>{
 
+const crearHospital = async(req, res) =>{
 const {nombre, descripcion, direccion, isActive, especialidad} = req.body
     if(!nombre|| !descripcion||!direccion||isActive === undefined||!especialidad){
     res.status(400).json({message: "faltan datos obligatorios"})
@@ -20,55 +19,66 @@ direccion: direccion,
 isActive: isActive,
 especialidad: especialidad
 })
-
 res.status(201).json({message: "hospital cargado de forma exitosa"})
 }
 
 
 const buscarPorEspecialidad = async (req, res) =>{
-especialidad = req.body.especialidad
-
+let especialidad = req.body.especialidad
 if(!especialidad){
     res.status(400).json({message: "faltan datos importantes"})
     return
 }
-
     const consulta = await Hospitales.findAll({
     where: {
 especialidad : especialidad
     }
-
 })
+if(consulta.length>0){
 res.status(200).json(consulta)
 
 }
+else{
+res.stauts(404).json({message: "no se encontro un hospital con esa especialidad"})
+}
+
+
+}
+
 
 const eliminarHospital = async (req, res) =>{
 const id = req.params.id
 const resultado = await Hospitales.destroy({
-
 where: {
 id : id
 }
-
-
-
 })
-
 if(resultado > 0){
-
 res.status(200).json("registro eliminado")
-
 }
 else {
-
     res.status(404).json("no se encontró un registro asi")
+}
+}
+
+const hospitalesActivos = async(req, res) =>{
+const resultado = await Hospitales.findAll({
+where: {
+    isActive : true
+}
+})
+if(resultado.length>0){
+
+res.status(200).json(resultado)
+}
+else{
+res.status(404).json({message: "no se encontraron hospitales activos"})
+}
+
 
 }
 
-}
 
 
 
-
-module.exports = { obtenerHospitales, crearHospital, buscarPorEspecialidad, eliminarHospital}
+module.exports = { obtenerHospitales, crearHospital, buscarPorEspecialidad, eliminarHospital, hospitalesActivos}
--- Sequelize/routes/hospitalesRutas.js
+++ Sequelize/routes/hospitalesRutas.js
@@ -1,4 +1,4 @@
-const { obtenerHospitales, crearHospital, buscarPorEspecialidad, eliminarHospital } = require("../controladores/hospitalesControladores")
+const { obtenerHospitales, crearHospital, buscarPorEspecialidad, eliminarHospital, hospitalesActivos } = require("../controladores/hospitalesControladores")
 const express = require("express")
 const router = express.Router()
 
@@ -6,5 +6,5 @@ router.get("/", obtenerHospitales)
 router.post("/crear", crearHospital)
 router.get("/buscarEspecialidad", buscarPorEspecialidad)
 router.get("/eliminarHospital/:id", eliminarHospital)
-
+router.get("/activos", hospitalesActivos)
 module.exports = router;
